6e6cd1c73499354a87984e8ed778a7f8e8d4ac9e
Raymundo Torres <torram_89@hotmail.com>
2022-11-16 17:32:47 -0600

    Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>My App</h1>
+            <button>boton 1</button>
+            <button>boton 2</button>
+            
+        </nav>
+    )
+}
+export default Navbar;
